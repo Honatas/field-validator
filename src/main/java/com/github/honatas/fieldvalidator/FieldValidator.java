aed2553b4edf3eb2686c2f4ba1229fb2e20fd0c9
Jonatas de Moraes Junior <jmoraes@gmail.com>
2026-02-23 14:38:58 -0300

    Added ability to pass object to validate

--- src/main/java/com/github/honatas/fieldvalidator/FieldValidator.java
+++ src/main/java/com/github/honatas/fieldvalidator/FieldValidator.java
@@ -1,25 +1,42 @@
 package com.github.honatas.fieldvalidator;
 
+import java.io.Serializable;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
 
-import org.apache.commons.lang3.StringUtils;
-
 import com.github.honatas.fieldvalidator.exception.FieldValidationException;
 
 /**
- * <p>Provide means for registering validators and validate data with them.</p>
+ * <p>Provides means for registering validators and validate data with them.</p>
  * 
- * @author Jonatas de Moraes Junior (honatas)
+ * @author Jonatas "Honatas" de Moraes Junior
  */
 public class FieldValidator {
 
-	private Map<String, String> errors = new HashMap<>();
+	protected Map<String, String> errors = new HashMap<>();
+	protected Serializable data;
+	protected Class<?> dataClazz;
+
+	public FieldValidator() {
+	}
+
+	public FieldValidator(Serializable data) {
+		this.data = data;
+		this.dataClazz = data.getClass();
+	}
 	
 	/** @return A {@code java.util.Map} conatining an error message for each field. */
 	public Map<String, String> getErrors() {
 		return this.errors;
+	}
+
+	/** @return The data that is being validated. */
+	public Serializable getData() {
+		return this.data;
 	}
 	
 	/** @return true if a validation error has occurred, false otherwise */
@@ -30,7 +47,7 @@ public class FieldValidator {
 	/** Checks if has any error and throws a {@code FieldValidationException} when errors are found. */
 	public void checkHasErrors() throws FieldValidationException {
 		if (this.hasErrors()) {
-			throw new FieldValidationException(this.errors);
+			throw new FieldValidationException(this.errors, this.data);
 		}
 	}
 	
@@ -67,6 +84,34 @@ public class FieldValidator {
 			}
 		}
 	}
+
+
+	public void validate(String fieldName, Validator... validators) {
+		Object value = getFieldValue(fieldName);
+		this.validate(fieldName, value, validators);
+	}
+
+
+
+	private Object getFieldValue(String fieldName) {
+		try {
+			return extractFieldValue(fieldName);
+		} catch (NoSuchFieldException | IllegalAccessException | InvocationTargetException e) {
+			return null;
+		}
+	}
+
+
+	private Object extractFieldValue(String fieldName) throws NoSuchFieldException, IllegalAccessException, InvocationTargetException {
+		try {
+			Method method = this.dataClazz.getMethod(fieldName)	;
+			return method.invoke(this.data);
+		} catch (NoSuchMethodException e) {
+			Field field = this.dataClazz.getDeclaredField(fieldName);
+			return field.get(this.data);
+		}
+	}
+
 	
 	/**
 	 * Creates a {@code Validator} by using a regular expression with the matches method.
@@ -77,7 +122,7 @@ public class FieldValidator {
 	 */
 	public static Validator getValidatorRegexMatch(String regex, String message) {
 		return (Object data) -> {
-			if (StringUtils.isEmpty((String) data)) {
+			if (data == null || ((String) data).isEmpty()) {
 				return null;
 			}
 			Pattern pattern = Pattern.compile(regex);
@@ -97,7 +142,7 @@ public class FieldValidator {
 	 */
 	public static Validator getValidatorRegexFind(String regex, String message) {
 		return (Object data) -> {
-			if (StringUtils.isEmpty((String) data)) {
+			if (data == null || ((String) data).isEmpty()) {
 				return null;
 			}
 			Pattern pattern = Pattern.compile(regex);
--- src/main/java/com/github/honatas/fieldvalidator/exception/FieldValidationException.java
+++ src/main/java/com/github/honatas/fieldvalidator/exception/FieldValidationException.java
@@ -1,5 +1,6 @@
 package com.github.honatas.fieldvalidator.exception;
 
+import java.io.Serializable;
 import java.util.Map;
 
 /**
@@ -9,14 +10,25 @@ public class FieldValidationException extends Exception {
 
 	private static final long serialVersionUID = -5275417944027609971L;
 
-	private Map<String, String> errors;
+	private final Map<String, String> errors;
+	private final Serializable data;
+
+	public FieldValidationException(Map<String, String> errors, Serializable data) {
+		this.errors = errors;
+		this.data = data;
+	}
 	
 	public FieldValidationException(Map<String, String> errors) {
-		this.errors = errors;
+		this(errors, null);
 	}
 	
 	/** @return A {@code Map} conatining an error message for each field. */
 	public Map<String, String> getErrors() {
 		return this.errors;
+	}
+
+	/** @return The data that was just validated. */
+	public Serializable getData() {
+		return this.data;
 	}
 }
